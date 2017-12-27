260e788bed84ed32580e0ca4803088a3b2835d07
bioprogram1316 <bioprogram1316@users.noreply.github.com>
2017-12-27 17:25:45 -0500

    Add files via upload
    This program reads in a set of moves within a string and decides whether the figure ends up in the same position that it started in, after making the moves.

---
+++ RouteCircleJudge.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+bool judgeCircle(string moves) {
+    int xCoord=0, yCoord=0;
+    bool isCircle=false;
+
+    for(int i=0; i<moves.length(); i++){
+        if(moves.at(i)=='R'){
+            xCoord++;
+        }
+        else if(moves.at(i)=='L'){
+            xCoord--;
+        }
+        else if(moves.at(i)=='U'){
+            yCoord--;
+        }
+        else if(moves.at(i)=='D'){
+            yCoord++;
+        }
+    }
+    if(xCoord==0 && yCoord==0)
+        isCircle=true;
+
+    return isCircle;
+}
+
+int main() {
+    string moves;
+
+    cin >> moves;
+
+    cout << judeCircle(moves) << endl;
+
+    return 0;
+}
